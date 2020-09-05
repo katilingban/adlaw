8e5f12123bd50775ba03ef119d84c12ad72879c4
Ernest Guevarra <ernest@guevarra.io>
2020-09-05 21:58:44 +0100

    update dataset documentation

--- R/data.R
+++ R/data.R
@@ -1,39 +1,16 @@
 ################################################################################
 #
-#' scalingupnutrition
-#'
-#' Scaling Up Nutrition (SUN) Movement Annual Progress Reports
-#'
-#' Scaling Up Nutrition (SUN) <http://scalingupnutrition.org> is a global movement
-#' of 60 countries aiming to end malnutrition in all its forms. The SUN Movement
-#' Annual Progress Report provides a detailed snapshot of the progress and results
-#' towards scaling up nutrition in SUN Countries and the broad evolution of the
-#' Movement. This package contains text dataset of the Movement's annual reports
-#' from 2011-2017 and various functions for processing, manipulating and analysing
-#' this dataset.
-#'
-#' @docType package
-#' @name scalingupnutrition
-#' @importFrom tibble as.tibble
-#'
-#
-################################################################################
-NULL
-
-
-################################################################################
-#
-#' progress_report_2011
 #'
 #' SUN movement annual progress report 2011
 #'
 #' @format A dataframe with 4 columns and 2209 rows:
-#' \describe{
-#' \item{\code{text}}{Line of text from the 2011 annual progress report}
-#' \item{\code{page}}{Page number to which line of text can be found}
-#' \item{\code{linenumber}}{Line number of text}
-#' \item{\code{chapter}}{Document chapter to which line of text can be found}
-#' }
+#'
+#' | **Variable** | **Description** |
+#' | :--- | :--- |
+#' | *text* | Line of text from the 2011 annual progress report |
+#' | *page* | Page number to which line of text can be found |
+#' | *linenumber* | Line number of text |
+#' | *chapter* | Document chapter to which line of text can be found |
 #'
 #' @source Scaling Up Nutrition Movement 2011
 #'
@@ -44,17 +21,17 @@ NULL
 
 ################################################################################
 #
-#' progress_report_2012
 #'
 #' SUN movement annual progress report 2012
 #'
 #' @format A dataframe with 4 columns and 2016 rows:
-#' \describe{
-#' \item{\code{text}}{Line of text from the 2012 annual progress report}
-#' \item{\code{page}}{Page number to which line of text can be found}
-#' \item{\code{linenumber}}{Line number of text}
-#' \item{\code{chapter}}{Document chapter to which line of text can be found}
-#' }
+#'
+#' | **Variable** | **Description** |
+#' | :--- | :--- |
+#' | *text* | Line of text from the 2012 annual progress report |
+#' | *page* | Page number to which line of text can be found |
+#' | *linenumber* | Line number of text |
+#' | *chapter* | Document chapter to which line of text can be found |
 #'
 #' @source Scaling Up Nutrition Movement 2012
 #'
@@ -65,17 +42,17 @@ NULL
 
 ################################################################################
 #
-#' progress_report_2013
 #'
 #' SUN movement annual progress report 2013
 #'
 #' @format A dataframe with 4 columns and 2915 rows:
-#' \describe{
-#' \item{\code{text}}{Line of text from the 2013 annual progress report}
-#' \item{\code{page}}{Page number to which line of text can be found}
-#' \item{\code{linenumber}}{Line number of text}
-#' \item{\code{chapter}}{Document chapter to which line of text can be found}
-#' }
+#'
+#' | **Variable** | **Description** |
+#' | :--- | :--- |
+#' | *text* | Line of text from the 2013 annual progress report |
+#' | *page* | Page number to which line of text can be found |
+#' | *linenumber* | Line number of text |
+#' | *chapter* | Document chapter to which line of text can be found |
 #'
 #' @source Scaling Up Nutrition Movement 2013
 #'
@@ -86,17 +63,17 @@ NULL
 
 ################################################################################
 #
-#' progress_report_2014
 #'
 #' SUN movement annual progress report 2014
 #'
 #' @format A dataframe with 4 columns and 3873 rows:
-#' \describe{
-#' \item{\code{text}}{Line of text from the 2014 annual progress report}
-#' \item{\code{page}}{Page number to which line of text can be found}
-#' \item{\code{linenumber}}{Line number of text}
-#' \item{\code{chapter}}{Document chapter to which line of text can be found}
-#' }
+#'
+#' | **Variable** | **Description** |
+#' | :--- | :--- |
+#' | *text* | Line of text from the 2014 annual progress report |
+#' | *page* | Page number to which line of text can be found |
+#' | *linenumber* | Line number of text |
+#' | *chapter* | Document chapter to which line of text can be found |
 #'
 #' @source Scaling Up Nutrition Movement 2014
 #'
@@ -107,17 +84,17 @@ NULL
 
 ################################################################################
 #
-#' progress_report_2015
 #'
 #' SUN movement annual progress report 2015
 #'
 #' @format A dataframe with 4 columns and 10886 rows:
-#' \describe{
-#' \item{\code{text}}{Line of text from the 2015 annual progress report}
-#' \item{\code{page}}{Page number to which line of text can be found}
-#' \item{\code{linenumber}}{Line number of text}
-#' \item{\code{chapter}}{Document chapter to which line of text can be found}
-#' }
+#'
+#' | **Variable** | **Description** |
+#' | :--- | :--- |
+#' | *text* | Line of text from the 2015 annual progress report |
+#' | *page* | Page number to which line of text can be found |
+#' | *linenumber* | Line number of text |
+#' | *chapter* | Document chapter to which line of text can be found |
 #'
 #' @source Scaling Up Nutrition Movement 2015
 #'
@@ -128,17 +105,17 @@ NULL
 
 ################################################################################
 #
-#' progress_report_2016
 #'
 #' SUN movement annual progress report 2016
 #'
 #' @format A dataframe with 4 columns and 8070 rows:
-#' \describe{
-#' \item{\code{text}}{Line of text from the 2016 annual progress report}
-#' \item{\code{page}}{Page number to which line of text can be found}
-#' \item{\code{linenumber}}{Line number of text}
-#' \item{\code{chapter}}{Document chapter to which line of text can be found}
-#' }
+#'
+#' | **Variable** | **Description** |
+#' | :--- | :--- |
+#' | *text* | Line of text from the 2016 annual progress report |
+#' | *page* | Page number to which line of text can be found |
+#' | *linenumber* | Line number of text |
+#' | *chapter* | Document chapter to which line of text can be found |
 #'
 #' @source Scaling Up Nutrition Movement 2016
 #'
@@ -149,17 +126,17 @@ NULL
 
 ################################################################################
 #
-#' progress_report_2017
 #'
 #' SUN movement annual progress report 2017
 #'
 #' @format A dataframe with 4 columns and 7586 rows:
-#' \describe{
-#' \item{\code{text}}{Line of text from the 2017 annual progress report}
-#' \item{\code{page}}{Page number to which line of text can be found}
-#' \item{\code{linenumber}}{Line number of text}
-#' \item{\code{chapter}}{Document chapter to which line of text can be found}
-#' }
+#'
+#' | **Variable** | **Description** |
+#' | :--- | :--- |
+#' | *text* | Line of text from the 2017 annual progress report |
+#' | *page* | Page number to which line of text can be found |
+#' | *linenumber* | Line number of text |
+#' | *chapter* | Document chapter to which line of text can be found |
 #'
 #' @source Scaling Up Nutrition Movement 2017
 #'
